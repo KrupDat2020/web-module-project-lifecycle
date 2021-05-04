897c69537f6307205dee25914b3f7ea843557ec3
KrupDat2020 <mark.krupinsky@gmail.com>
2021-05-04 14:44:36 -0500

    having react issues

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+  return (
+    <div>
+    <h1>Hello</h1>
+  );
+}
+}
+
+export default App;
